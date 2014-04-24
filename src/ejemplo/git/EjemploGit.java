9e477cbe6f7c239b03aeabbd5273d229d28d1829
jeffaristi92 <jeff.aristi@gmail.com>
2014-04-24 00:03:39 -0500

    Creacion de atributo 3

--- src/ejemplo/git/EjemploGit.java
+++ src/ejemplo/git/EjemploGit.java
@@ -16,6 +16,8 @@ public class EjemploGit {
     
     private int atributo1;
     private int atributo2;
+    
+    private int atributo3;
     public static void main(String[] args) {
         // TODO code application logic here
     }
