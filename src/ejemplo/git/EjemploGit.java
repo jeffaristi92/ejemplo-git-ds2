3f687f43e1628e0825344525e6628f48e34d67c2
jeffaristi92 <jeff.aristi@gmail.com>
2014-04-24 00:14:14 -0500

    Revirtiento creación de atributos

--- src/ejemplo/git/EjemploGit.java
+++ src/ejemplo/git/EjemploGit.java
@@ -13,11 +13,6 @@ public class EjemploGit {
     /**
      * @param args the command line arguments
      */
-    
-    private int atributo1;
-    private int atributo2;
-    
-    private int atributo3;
     public static void main(String[] args) {
         // TODO code application logic here
     }
